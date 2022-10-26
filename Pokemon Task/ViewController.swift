4419dbf3516ede90d34027a84752f5b3103e66d9
shalai-ivan <shalai.ivan30@gmail.com>
2022-10-26 16:19:16 +0300

    Initial Commit

---
+++ Pokemon Task/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pokemon Task
+//
+//  Created by MacMini on 26.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
